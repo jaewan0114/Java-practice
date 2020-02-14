fcadffb014dad730d00ae5a2db0404eac5bc8193
jaewan0114 <42615916+jaewan0114@users.noreply.github.com>
2020-02-14 17:56:19 +0900

    백준 11718

---
+++ n_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class n_1 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner scan = new Scanner(System.in);
+        
+        for(int i=0; i<100; i++) {
+            
+            String word = scan.nextLine();
+            System.out.println(word);
+            
+        }   
+    }
+}
